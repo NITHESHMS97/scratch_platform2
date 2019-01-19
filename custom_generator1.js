725a017c5f3c9096018b31a4bcbd67fef564ff45
NITHESHMS97 <nitheshms97@gmail.com>
2019-01-19 17:30:18 +0530

    converted all return code into , fromat repeat has an issue

--- custom_generator1.js
+++ custom_generator1.js
@@ -17,25 +17,25 @@ Blockly.JavaScript['A1']=function(block)
 		this.getInput('VALUE').setCheck('String')
 	else	
 		this.getInput('VALUE').setCheck('Number')
-	var code="1"
+	var code="1,"
 	if(targetBlock)
 	{
 		var value=targetBlock.getFieldValue('VAL');
 		if(value=='HIGH')
-			code+="01";
+			code+="0,1,";
 		else if(value=='LOW')
-			code+="00";
+			code+="0,0,";
 		else
 		{
 			first16=value & 65280;
 			last16=value & 255;
 			first16=first16>>8;
-			code+=first16.toString();
-			code+=last16.toString();
+			code+=first16.toString()+",";
+			code+=last16.toString()+",";
 		}
 		
 	}
-	msg1=" "+code;
+	msg1=code;
 	printIt();
 	return code;
 	
@@ -52,21 +52,21 @@ Blockly.JavaScript['A2']=function(block)
 		this.getInput('VALUE').setCheck('String')
 	else	
 		this.getInput('VALUE').setCheck('Number')
-	var code="2"
+	var code="2,"
 	if(targetBlock)
 	{
 		var value=targetBlock.getFieldValue('VAL');
 		if(value=='HIGH')
-			code+="01";
+			code+="0,1,";
 		else if(value=='LOW')
-			code+="00";
+			code+="0,0,";
 		else
 		{
 			first16=value & 65280;
 			last16=value & 255;
 			first16=first16>>8;
-			code+=first16.toString();
-			code+=last16.toString();
+			code+=first16.toString()+",";
+			code+=last16.toString()+",";
 		}
 		
 	}
@@ -84,8 +84,17 @@ Blockly.JavaScript['wait']=function(blocks)
 	var hrsmili=parseInt(hrs)*60*60*1000;
 	var minmili=parseInt(min)*60*1000;
 	var secmili=parseInt(sec)*1000;
+	var time=hrsmili+minmili+secmili;
+	byte4=time & 4278190080;
+	byte3=time & 16711680;
+	byte2=time & 65280;
+	byte1=time & 255;
+	byte2>>=8;
+	byte3>>=16;
+	byte4>>=24;
 	msg3=" }w"+(hrsmili+minmili+secmili).toString();
-	return " w"+(hrsmili+minmili+secmili).toString();
+//	return "w,"+(hrsmili+minmili+secmili).toString();
+	return "w,"+byte4.toString()+","+byte3.toString()+","+byte2.toString()+","+byte1.toString()+",";
 
 }
 
@@ -114,7 +123,7 @@ function generateByteformat(value)
 		firstbyte=first16.toString();
 		lastbyte=last16.toString();	
 	}
-	return firstbyte+lastbyte;
+	return firstbyte+","+lastbyte+",";
 }
 
 
@@ -128,9 +137,13 @@ Blockly.JavaScript['if_do']=function(blocks)
 	{
 		var value1;
 		var value2;
+		var portval;
 		var port=conditionBlock.getFieldValue('PORT');
 		var operator=conditionBlock.getFieldValue('OP');
-		
+		if(port=="A1")
+			portval="1,";
+		else if(port=="A2")
+			portval="2,";
 //		conditionBlock.getField("VAL2").setVisible(false);
 		var value1=conditionBlock.getFieldValue("VAL1");
 		var value2=0;
@@ -145,9 +158,9 @@ Blockly.JavaScript['if_do']=function(blocks)
 		var value1byte=generateByteformat(value1);
 		var value2byte=generateByteformat(value2);
 //		alert(value2byte)
-		
-		code="d";
-		code+=value2byte+port+value1byte+operator;
+		var opascii=operator.charCodeAt().toString();
+		code="d,";
+		code+=value2byte+portval+value1byte+opascii+",";
 		children=blocks.getChildren(false);
 		//if(children[0])
 		//	child += Blockly.JavaScript.blockToCode(children[0]);
@@ -171,11 +184,14 @@ Blockly.JavaScript['repeat']=function(blocks)
 	var count=0;
 	if(countBlock)
 		count=countBlock.getFieldValue('VAL');
+	
 	var children=blocks.getChildren();
 	var child="";
 	if(children[0])
 		child=Blockly.JavaScript.blockToCode(children[0]);
-	return code+count.toString()+child;
+//	return code+","+count.toString()+","+child;
+	repeatf.innerHTML=children[0].type;
+	return code+","+count.toString()+","+child;
 }
 
 Blockly.JavaScript['start']=function(block)
@@ -189,7 +205,7 @@ Blockly.JavaScript['start']=function(block)
 		x=children[0].type;
 		code=Blockly.JavaScript.blockToCode(children[0]);
 		startgen.innerHTML=code;
-		
+
 	}
 	return "";
 }
